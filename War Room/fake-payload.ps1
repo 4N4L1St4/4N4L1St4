13baecb5783ecb213473f327592b33f7ee4f6ef6
analistagabriel <controleti.001.003@gmail.com>
2025-08-07 16:58:44 -0300

    ajustes

--- War Room/fake-payload.ps1
+++
@@ -1,6 +0,0 @@
-# fake-payload.ps1
-
-# Comando simulado (ex.: abrir bloco de notas)
-$cmd = 'Start-Process notepad'
-$enc = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($cmd))
-powershell.exe -EncodedCommand $enc
